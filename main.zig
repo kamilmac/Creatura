e069992fc4b8cf3f39fe16b5ca37c3ba7b45736e
kamilmac <kmacinski@gmail.com>
2024-07-27 12:01:40 +0200

    it works

--- main.zig
+++ main.zig
@@ -3,33 +3,34 @@ const std = @import("std");
 extern fn updateTexture(data: [*]const u8, width: u32, height: u32) void;
 
 pub fn main() void {
-    const newData = [_] u8 {
-        ...getRandomPixel(),
-        ...getRandomPixel(),
-        ...getRandomPixel(),
-        ...getRandomPixel(),
-        ...getRandomPixel(),
-        ...getRandomPixel(),
-        ...getRandomPixel(),
-        ...getRandomPixel(),
-        ...getRandomPixel(),
-        ...getRandomPixel(),
-        ...getRandomPixel(),
-        ...getRandomPixel(),
-        ...getRandomPixel(),
-        ...getRandomPixel(),
-        ...getRandomPixel(),
-        ...getRandomPixel(),
-    };
+    // const newData = [_] u8 {
+    //     ...getRandomPixel(),
+    //     ...getRandomPixel(),
+    //     ...getRandomPixel(),
+    //     ...getRandomPixel(),
+    //     ...getRandomPixel(),
+    //     ...getRandomPixel(),
+    //     ...getRandomPixel(),
+    //     ...getRandomPixel(),
+    //     ...getRandomPixel(),
+    //     ...getRandomPixel(),
+    //     ...getRandomPixel(),
+    //     ...getRandomPixel(),
+    //     ...getRandomPixel(),
+    //     ...getRandomPixel(),
+    //     ...getRandomPixel(),
+    //     ...getRandomPixel(),
+    // };
 
-    updateTexture(newData[0..], 4, 4);
+    // updateTexture(newData[0..], 4, 4);
 }
 
 fn getRandomPixel() []u8 {
-    return [
-        std.math.random() % 256,
-        std.math.random() % 256,
-        std.math.random() % 256,
+    var rng = std.rand.DefaultPrng.init(std.time.nanoTimestamp());
+    return &[_]u8{
+        @intCast(rng.random() % 256),
+        @intCast(rng.random() % 256),
+        @intCast(rng.random() % 256),
         255,
-    ];
+    };
 }
