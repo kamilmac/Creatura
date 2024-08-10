caf96e0bce304e6c6d3ebae3bde4de26640c15c2
kamilmac <kmacinski@gmail.com>
2024-08-10 11:32:02 +0200

    +

--- main.zig
+++ main.zig
@@ -2,6 +2,12 @@ const std = @import("std");
 
 extern "env" fn js_console_log(ptr: [*]const u8, len: usize) void;
 
+const MAX_POINTS = 1024;
+const MAX_FORCES = 512;
+
+var rng = std.rand.DefaultPrng.init(0);
+var app: App = undefined;
+
 const Canvas = struct {
     height: u32,
     width: u32,
@@ -10,6 +16,13 @@ const Canvas = struct {
 const Point = struct {
     x: f32,
     y: f32,
+
+    pub fn init(x: f32, y: f32) Point {
+        return .{
+            .x = x,
+            .y = y,
+        };
+    }
 };
 
 const AttractorForce = struct {
@@ -68,28 +81,39 @@ const Force = union(enum) {
 };
 
 const App = struct {
-    points: []Point,
-    forces: []Force,
+    allocator: std.mem.Allocator,
+    points: [MAX_POINTS]Point,
+    forces: [MAX_FORCES]Force,
+    point_count: u16,
+    force_count: u16,
     buffer: []u8,
     canvas: Canvas,
 
-    pub fn init(allocator: std.mem.Allocator, width: u32, height: u32, num_points: usize, num_forces: usize) !App {
-        const points = try allocator.alloc(Point, num_points);
-        const forces = try allocator.alloc(Force, num_forces);
+    pub fn init(allocator: std.mem.Allocator, width: u32, height: u32) !App {
         const buffer = try allocator.alloc(u8, width * height * 4);
-
         return App{
-            .points = points,
-            .forces = forces,
+            .allocator = allocator,
+            .points = undefined,
+            .forces = undefined,
+            .point_count = 0,
+            .force_count = 0,
             .buffer = buffer,
             .canvas = Canvas{ .width = width, .height = height },
         };
     }
 
-    pub fn deinit(self: *App, allocator: std.mem.Allocator) void {
-        allocator.free(self.points);
-        allocator.free(self.forces);
-        allocator.free(self.buffer);
+    pub fn addPoint(self: *App, x: f32, y: f32) *Point {
+        // if (self.point_count >= MAX_POINTS) return error.TooManyPoints;
+        const index = self.point_count;
+        self.points[index] = Point.init(x, y);
+        self.point_count += 1;
+        return &self.points[index];
+    }
+
+    pub fn deinit(self: *App) void {
+        // self.allocator.free(self.points);
+        // self.allocator.free(self.forces);
+        self.allocator.free(self.buffer);
     }
 
     pub fn clearBuffer(self: *App) void {
@@ -118,37 +142,30 @@ const App = struct {
     }
 };
 
-var rng = std.rand.DefaultPrng.init(0);
-var app: App = undefined;
-
 export fn init(width: u32, height: u32) void {
     const allocator = std.heap.page_allocator;
-    app = App.init(allocator, width, height, 128, 4) catch unreachable;
+    app = App.init(allocator, width, height) catch unreachable;
+    defer app.deinit();
 
-    app.points[0] = .{ .x = 0.0, .y = 0.0 };
-    app.points[1] = .{ .x = 0.6, .y = 0.6 };
-    app.points[2] = .{ .x = 0.0, .y = 0.6 };
-    app.points[3] = .{ .x = 0.6, .y = -0.6 };
-    app.forces[0] = Force{ .Attractor = AttractorForce.init(&app.points[1], app.points[0..1]) };
-    app.forces[1] = Force{ .Attractor = AttractorForce.init(&app.points[2], app.points[0..1]) };
-    app.forces[2] = Force{ .Attractor = AttractorForce.init(&app.points[3], app.points[0..1]) };
+    _ = app.addPoint(0.0, 0.0);
+
+    app.forces[0] = Force{ .Attractor = AttractorForce.init(app.addPoint(0.6, 0.6), app.points[0..1]) };
+    app.forces[1] = Force{ .Attractor = AttractorForce.init(app.addPoint(0.0, 0.6), app.points[0..1]) };
+    app.forces[2] = Force{ .Attractor = AttractorForce.init(app.addPoint(0.6, -0.6), app.points[0..1]) };
+
     app.forces[3] = Force{ .Wind = WindForce.init(0.001, app.points[1..2]) };
-
+    app.force_count = 4;
     app.clearBuffer();
 }
 
-export fn go(timeSinceStart: f32) [*]const u8 {
-    if (timeSinceStart > 8000) {
-        // You might want to add some behavior here
-    }
-
+export fn go() [*]const u8 {
     for (app.points) |point| {
         app.drawPointToBuffer(point.x, point.y, 255);
     }
 
-    for (app.forces) |*force| {
-        force.process();
-        switch (force.*) {
+    for (app.forces[0..app.force_count], 0..) |force, i| {
+        app.forces[i].process();
+        switch (force) {
             .Attractor => |attractor| app.drawPointToBuffer(attractor.origin.x, attractor.origin.y, 0),
             .Wind => {}, // Wind forces are not drawn
         }
@@ -165,8 +182,4 @@ fn logInt(value: i32) void {
     var buf: [32]u8 = undefined;
     const formatted = std.fmt.bufPrint(&buf, "{d}", .{value}) catch return;
     log(formatted);
-}
-
-export fn deinit() void {
-    app.deinit(std.heap.page_allocator);
 }
