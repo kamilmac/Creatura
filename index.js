a91aa954c7dffc441df10f6f2fc7a71fc8ea2b88
kamilmac <kmacinski@gmail.com>
2024-09-03 12:48:07 +0200

    +

--- index.js
+++ index.js
@@ -102,8 +102,20 @@ function initWebGL() {
 
   canvas.addEventListener('mousemove', (event) => {
     const rect = canvas.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
     mouseX = 2 * ((event.clientX - rect.left) / CANVAS_WIDTH - 0.5);
     mouseY = -2 * (1 - (event.clientY - rect.top) / CANVAS_HEIGHT - 0.5); // Invert Y-axis
+
+    const rotateX = -((event.clientY - centerY) / (rect.height / 2)) * 8; // Max 10 degrees
+    const rotateY = ((event.clientX - centerX) / (rect.width / 2)) * 8; // Max 10 degrees
+
+    const shadowX = (event.clientX - centerX) / 22;
+    const shadowY = (event.clientY - centerY) / 22;
+    const shadowBlur = Math.sqrt(shadowX * shadowX + shadowY * shadowY);
+
+    canvas.style.boxShadow = `${shadowX}px ${shadowY}px ${shadowBlur}px rgba(0,0,0,0.2)`;
+    canvas.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
   });
 
   canvas.addEventListener('click', (event) => {
